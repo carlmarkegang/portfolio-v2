616caadd849df48c29385a962a4a07ff131dbd37
Carl Markegång <huledule@hotmail.com>
2025-03-10 10:56:06 +0100

    CV

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Homepage() {
 
       <br />
       <div>
-        <a href="/cv.pdf" target="_blank">Se mitt CV</a>
+        <a href="/cv.pdf" target="_blank">Se mitt CV som PDF</a>
       </div>
 
       <div className="bottomnav">
