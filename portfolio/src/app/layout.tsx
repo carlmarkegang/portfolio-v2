20b65920e049962b978e87beada667f92405bfb1
carlmarkegang <huledule@hotmail.com>
2025-03-20 16:15:34 +0100

    gtag

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-BPX3LGXG6E"></script>
+      <script src="/gtag.js"></script>
       </head>
       <body>
         <div className="portfolio-main-wrap">
